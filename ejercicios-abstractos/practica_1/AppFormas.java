60b99bb1cbfd8d9a7a3d4f16e042d476cfa5124f
Beto <betoml5@hotmail.com>
2021-04-26 20:34:37 -0500

    Practica-1 Done

---
+++ ejercicios-abstractos/practica_1/AppFormas.java
@@ -0,0 +1,23 @@
+package practica_1;
+
+import javax.swing.JOptionPane;
+
+/**
+ * AppFormas
+ */
+public class AppFormas {
+
+    public static void main(String[] args) {
+        Rectangulo miRectangulo = new Rectangulo(10, 40);
+        Circulo miCirculo = new Circulo(20);
+
+        miRectangulo.calcularArea();
+        miCirculo.calcularArea();
+
+        JOptionPane.showMessageDialog(null, miRectangulo.getArea(), "El area del rectangulo", 1);
+        JOptionPane.showMessageDialog(null, miCirculo.getArea(), "El area del circulo", 1);
+
+        System.out.println(miRectangulo.getArea());
+        System.out.println(miCirculo.getArea());
+    }
+}
